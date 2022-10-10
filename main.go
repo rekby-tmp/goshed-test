103de8bebdd34fd58036d22b3203508126044517
Timofey Koolin <timofey.koolin@gmail.com>
2022-10-10 08:37:31 +0300

    stat

--- main.go
+++ main.go
@@ -6,32 +6,31 @@ import (
 	"net/http/pprof"
 	"runtime"
 	"sync"
+	"sync/atomic"
 	"time"
+
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 const (
-	goroutinesCount    = 1
+	goroutinesCount    = 1000
 	iterationTimeout   = time.Second * 10 * 1000
 	counterDotInterval = 10
-	counterNewLine     = counterDotInterval * 100
-	lockCount          = 1000
+	statInterval       = counterDotInterval * 100
+	lockCount          = 1
 	testDuration       = time.Minute * 10
 )
 
 func iteration() error {
 	var wg sync.WaitGroup
 
+	s := int64(0)
+
 	for i := 0; i < goroutinesCount; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-
-			var m sync.Mutex
-			for k := 0; k < lockCount; k++ {
-				m.Lock()
-				m.Unlock()
-			}
-
+			atomic.AddInt64(&s, 1)
 		}()
 	}
 
@@ -79,7 +78,14 @@ func main() {
 		if counter%counterDotInterval == 0 {
 			fmt.Print(".")
 		}
-		if counter%counterNewLine == 0 {
+		if counter%statInterval == 0 {
+			fmt.Println()
+
+			MB := uint64(1024 * 1024)
+			swap, _ := mem.SwapMemory()
+			fmt.Printf("swap free: %d, used: %d", swap.Free/MB, swap.Used/MB)
+			virt, _ := mem.VirtualMemory()
+			fmt.Printf("mem free: %d, used: %d", virt.Free/MB, virt.Used/MB)
 			fmt.Println()
 		}
 	}
